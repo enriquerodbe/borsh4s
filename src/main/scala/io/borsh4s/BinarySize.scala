94b3fa1cf93c21bdf6b8e793c04372f1ae6a460c
Enrique Rodríguez <enriquerodbe@gmail.com>
2022-05-19 20:39:02 -0300

    Implement auto derivation using Magnolia

--- src/main/scala/io/borsh4s/BinarySize.scala
+++ src/main/scala/io/borsh4s/BinarySize.scala
@@ -1,5 +1,20 @@
 package io.borsh4s
 
+import magnolia1.*
+
 trait BinarySize[T] {
   def calculate(t: T): Int
+}
+
+object BinarySize {
+  type Typeclass[T] = BinarySize[T]
+
+  def join[T](ctx: CaseClass[BinarySize, T]): BinarySize[T] =
+    (t: T) =>
+      ctx.parameters.map(p => p.typeclass.calculate(p.dereference(t))).sum
+
+  def split[T](ctx: SealedTrait[BinarySize, T]): BinarySize[T] =
+    (t: T) => 1 + ctx.split(t)(sub => sub.typeclass.calculate(sub.cast(t)))
+
+  implicit def gen[T]: BinarySize[T] = macro Magnolia.gen[T]
 }
